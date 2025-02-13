231970e48de83337c1b1f1b33da1cfff8fc6683b
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-02-13 16:54:18 +0100

    first commit

---
+++ conference-ticket-generator/src/App.jsx
@@ -0,0 +1,14 @@
+
+// import TicketSelection from "./components/TicketSelection"
+// import AttendeeForm from "./components/ AttendeeForm"
+import Ticket from "./components/Ticket"
+
+export default function App() {
+  return (
+    <div>
+      {/* <TicketSelection  /> */}
+      <Ticket/>
+      {/* <AttendeeForm /> */}
+    </div>
+  )
+}
