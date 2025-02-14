4c2c6c67388026e7c1ea0ccd474043a09a589b4d
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-02-14 13:13:04 +0100

    Did my routing

--- conference-ticket-generator/src/App.jsx
+++ conference-ticket-generator/src/App.jsx
@@ -1,14 +1,17 @@
-
-// import TicketSelection from "./components/TicketSelection"
-// import AttendeeForm from "./components/ AttendeeForm"
-import Ticket from "./components/Ticket"
+import TicketSelection from "./components/TicketSelection";
+import AttendeeForm from "./components/ AttendeeForm";
+import Ticket from "./components/Ticket";
+import { Routes, Route } from "react-router";
 
 export default function App() {
   return (
-    <div>
-      {/* <TicketSelection  /> */}
-      <Ticket/>
-      {/* <AttendeeForm /> */}
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<TicketSelection />} />
+        <Route path="attendanceform" element={<AttendeeForm />} />
+        <Route path ="ticket" element={ <Ticket />}/>
+       
+      </Routes>
     </div>
-  )
+  );
 }
